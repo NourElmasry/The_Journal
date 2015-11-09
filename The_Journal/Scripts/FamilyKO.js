985b2ffe6ca4f6873bcb722f46a68e3bdb6e6e87
nourelmasry <nourelmasri@gmail.com>
2015-11-09 07:27:03 +0000

    Applying Knockout js

---
+++ The_Journal/Scripts/FamilyKO.js
@@ -0,0 +1,27 @@
+﻿//var viewModel = {
+
+//    ContactID: '',
+//    CFirstName: 'Nour',
+//    CLastName: '',
+//    CDOB: '',
+//    CHomeNum: '',
+//    CWorkNum: '',
+//    CMobileNum: '',
+//    CAddress: '',
+//    CPostCode: '',
+//    EmergencyContactID: '',
+//    EmFirstName: '',
+//    EmLastName: '',
+//    EmMobileNum: '',
+//    EmRelationship: '',
+//    addCarer: '',
+//    addEC:''
+//};
+
+function AddCarer(CFirstName, CLastName, CLastName, CDOB, CHomeNum, CWorkNum, CMobileNum, CAddress, CPostCode)
+{
+
+}
+
+
+ko.applyBindings(viewModel);
--- The_Journal/ViewModels/FamilyViewModel.cs
+++ The_Journal/ViewModels/FamilyViewModel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;    
 using System.Data.Entity.Spatial;
 using System.ComponentModel.DataAnnotations.Schema;
 using The_Journal.Models;
